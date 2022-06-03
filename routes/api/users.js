4a6eda4a5ab5a3eb2c5ead03dea825f3ea8d8729
smansfield5 <smansfield5@yahoo.com>
2022-06-03 16:04:21 -0600

    Initial setup

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-// GET /api/users/check-token
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 // POST /api/users
 router.post('/', usersCtrl.create);
 // POST /api/users/login
